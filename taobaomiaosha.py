a392da3bf89182b04b82c529b60d7b7820e0a5ca
jiajiu123 <60831923+jiajiu123@users.noreply.github.com>
2024-10-18 16:03:10 +0800

    Update taobaomiaosha.py

--- taobaomiaosha.py
+++ taobaomiaosha.py
@@ -6,32 +6,48 @@ from DrissionPage import ChromiumPage
 page = ChromiumPage()
 
 # 指定秒杀时间（转换为datetime对象）
-times = datetime.datetime.strptime("2024-10-10 20:00:00.000000", '%Y-%m-%d %H:%M:%S.%f')
-
+while True:
+    try:
+        times = datetime.datetime.strptime(
+            input("请输入抢购时间 格式为YYYY-MM-DD HH:mm\n"), "%Y-%m-%d %H:%M"
+        )
+    except ValueError:
+        print("格式错误，请重新输入")
+    else:
+        break
 # 打开淘宝网页
 page.get("https://www.taobao.com")
-# 点击购物车
-page.ele('x://*[@id="J_MiniCart"]/div[1]/a/span[2]').click()
-# 等待登录完成，直到购物车全选按钮出现，超时时间我设置为1分钟
-page.wait.ele_displayed('@class=ant-checkbox-wrapper cartOperationCheckbox--CIlk23mK', timeout=60)
-# 点击购物车全选按钮
-page.ele('@class=ant-checkbox-wrapper cartOperationCheckbox--CIlk23mK').click()
-
+wait_for_buy = False
 # 无限循环检测时间
 while True:
     # 获取当前时间
     now = datetime.datetime.now()
-
-    # 打印当前时间用于调试
-    print(now.strftime('%Y-%m-%d %H:%M:%S.%f'))  
-
+    # 每隔一分钟打印当前时间并刷新当前页面，当间隔小于10分钟则不输出时间
+    time_difference = (times - now).seconds
+    if now < times:
+        if time_difference > 600:
+            print(f"当前时间：{now.strftime('%Y-%m-%d %H:%M:%S')}")
+            page.refresh()
+            time.sleep(60)
+    if not wait_for_buy:
+        print("当前间隔小于10分钟，不输出时间，等待抢购")
+        page.refresh()
+        # 点击购物车
+        page.ele("x://*[@id='J_MiniCart']/div[1]/a/span[2]").click()
+        # 等待登录完成，直到购物车全选按钮出现，超时时间我设置为1分钟
+        page.wait.ele_displayed("@class=ant-checkbox-input", timeout=60)
+        # 点击购物车全选按钮
+        page.ele("@class=ant-checkbox-input").click()
+        wait_for_buy = True
     # 如果当前时间大于等于指定时间，执行秒杀
     if now >= times:
         while True:
             try:
                 # 找到结算按钮并点击
-                if page.ele('x://*[@id="settlementContainer_1"]/div[4]/div/div[2]'):
-                    page.ele('x://*[@id="settlementContainer_1"]/div[4]/div/div[2]').click()
+                if page.ele("x://*[@id='settlementContainer_1']/div[4]/div/div[2]"):
+                    page.ele(
+                        "x://*[@id='settlementContainer_1']/div[4]/div/div[2]"
+                    ).click()
                     print("结算成功")
                     break
             except:
@@ -39,9 +55,9 @@ while True:
         while True:
             try:
                 # 找到提交订单按钮并点击
-                if page.ele('x://*[@id="submitOrder"]/div/div[2]/div'):
-                    page.ele('x://*[@id="submitOrder"]/div/div[2]/div').click()
-                    now1 = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+                if page.ele("x://*[@id='submitOrder']/div/div[2]/div"):
+                    page.ele("x://*[@id='submitOrder']/div/div[2]/div").click()
+                    now1 = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
                     print("抢购成功时间：%s" % now1)
                     break
             except:
