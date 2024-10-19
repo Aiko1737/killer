784656c2032511f9d0854b5e01a1fef3c2de9d9d
jiajiu123 <60831923+jiajiu123@users.noreply.github.com>
2024-10-20 02:00:46 +0800

    Update taobaomiaosha.py

--- taobaomiaosha.py
+++ taobaomiaosha.py
@@ -22,25 +22,28 @@ wait_for_buy = False
 while True:
     # 获取当前时间
     now = datetime.datetime.now()
-    # 每隔一分钟打印当前时间并刷新当前页面，当间隔小于10分钟则不输出时间
-    time_difference = (times - now).seconds
+    time_diff = (times - now).seconds
+    # 当前时间小于等于指定时间
     if now < times:
-        if time_difference > 600:
+        # 当间隔大于10分钟时，每隔一分钟打印当前时间并刷新当前页面
+        if time_diff > 600:
             print(f"当前时间：{now.strftime('%Y-%m-%d %H:%M:%S')}")
             page.refresh()
             time.sleep(60)
-    if not wait_for_buy:
-        print("当前间隔小于10分钟，不输出时间，等待抢购")
-        page.refresh()
-        # 点击购物车
-        page.ele("x://*[@id='J_MiniCart']/div[1]/a/span[2]").click()
-        # 等待登录完成，直到购物车全选按钮出现，超时时间我设置为1分钟
-        page.wait.ele_displayed("@class=ant-checkbox-input", timeout=60)
-        # 点击购物车全选按钮
-        page.ele("@class=ant-checkbox-input").click()
-        wait_for_buy = True
+        # 当间隔小于10分钟不输出时间，等待抢购
+        elif not wait_for_buy:
+            print(time_diff)
+            print("当前间隔小于10分钟，不输出时间，等待抢购")
+            page.refresh()
+            # 点击购物车
+            page.ele("x://*[@id='J_MiniCart']/div[1]/a/span[2]").click()
+            # 等待登录完成，直到购物车全选按钮出现，超时时间我设置为1分钟
+            page.wait.ele_displayed("@class=ant-checkbox-input", timeout=60)
+            # 点击购物车全选按钮
+            page.ele("@class=ant-checkbox-input").click()
+            wait_for_buy = True
     # 如果当前时间大于等于指定时间，执行秒杀
-    if now >= times:
+    else:
         while True:
             try:
                 # 找到结算按钮并点击
